3c1fdfded2487ec0b26dede4593a1f753bf44b95
Yasser Al-Najjar <yasseralnajjar@outlook.sa>
2024-04-23 02:04:50 +0200

    creating containers

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 const toggle = document.querySelector(".toggle_mode");
 toggle.addEventListener("click", () => {
   document.body.classList.toggle("dark");
+  toggle.innerText = document.body.classList.contains("dark") ? "🌕" : "🌑";
 });
